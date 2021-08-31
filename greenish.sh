7aae807fc118bb7610134754d9e5f6b984738581
Vivek-mishra-2002 <vivekmishra4002@gmail.com>
2021-09-01 01:00:00 +0530

    Snow Day -- stay home.

--- greenish.sh
+++ greenish.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #echo $var
 for ((DAYS = 1 ; DAYS <=31 ; DAYS++)); do
-    sudo date -s "2021-10-$DAYS"
-    var=$((3 + $RANDOM % 25))
+    sudo date -s "2021-9-$DAYS"
+    var=$((3 + $RANDOM % 22))
     for ((HOURS = 1 ; HOURS < $var ; HOURS++)); do
         echo HOURS $HOURS
-        sudo date -s "2021-10-$DAYS $HOURS:00:00"
+        sudo date -s "2021-9-$DAYS $HOURS:00:00"
         echo `/usr/games/fortune -sn 32` > vivek.txt
         git add .
         git commit -m "`/usr/games/fortune -sn 32`"
