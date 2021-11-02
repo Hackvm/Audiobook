a2bbae24adf33efca16741cebe7903b7f971c238
Vivek-mishra-2002 <vivekmishra4002@gmail.com>
2021-11-03 01:00:00 +0530

    It's all in the mind, ya know.

--- greenish.sh
+++ greenish.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-var=$((1 + $RANDOM % 10))
+var=$((10 + $RANDOM % 13))
 #echo $var
 
-for ((DAYS = 1 ; DAYS <= 30 ; DAYS++)); do
-    sudo date -s "2021-10-$DAYS"
+for ((DAYS = 3 ; DAYS <= 30 ; DAYS++)); do
+    sudo date -s "2021-11-$DAYS"
     for ((HOURS = 1 ; HOURS < $var ; HOURS++)); do
         echo HOURS $HOURS
-        sudo date -s "2021-10-$DAYS $HOURS:00:00"
+        sudo date -s "2021-11-$DAYS $HOURS:00:00"
         echo `/usr/games/fortune -sn 32` > vivek.txt
         git add .
         git commit -m "`/usr/games/fortune -sn 32`"
