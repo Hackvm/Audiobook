428f93d7496619b55e588134d1880ba4c9ce7cee
Vivek-mishra-2002 <vivekmishra4002@gmail.com>
2021-12-10 01:00:00 +0530

    You too can wear a nose mitten.

--- greenish.sh
+++ greenish.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-var=$((10 + $RANDOM % 13))
+var=$((1 + $RANDOM % 24))
 #echo $var
 
-for ((DAYS = 3 ; DAYS <= 30 ; DAYS++)); do
-    sudo date -s "2021-11-$DAYS"
+for ((DAYS = 10 ; DAYS <= 31 ; DAYS++)); do
+    sudo date -s "2021-12-$DAYS"
     for ((HOURS = 1 ; HOURS < $var ; HOURS++)); do
         echo HOURS $HOURS
-        sudo date -s "2021-11-$DAYS $HOURS:00:00"
+        sudo date -s "2021-12-$DAYS $HOURS:00:00"
         echo `/usr/games/fortune -sn 32` > vivek.txt
         git add .
         git commit -m "`/usr/games/fortune -sn 32`"
