7e2e9b0b78a481f8cd754525b92d639308824772
Vivek-mishra-2002 <vivekmishra4002@gmail.com>
2022-01-01 01:00:00 +0530

    Advancement in position.

--- greenish.sh
+++ greenish.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-var=$((1 + $RANDOM % 30))
+var=$((5 + $RANDOM % 14))
 #echo $var
-
-for ((DAYS = 10 ; DAYS <= 31 ; DAYS++)); do
-    sudo date -s "2021-12-$DAYS"
+for ((DAYS = 1 ; DAYS <= 31 ; DAYS++)); do
+    sudo date -s "2022-01-$DAYS"
     for ((HOURS = 1 ; HOURS < $var ; HOURS++)); do
         echo HOURS $HOURS
-        sudo date -s "2021-12-$DAYS $HOURS:00:00"
+        sudo date -s "2022-01-$DAYS $HOURS:00:00"
         echo `/usr/games/fortune -sn 32` > vivek.txt
         git add .
         git commit -m "`/usr/games/fortune -sn 32`"
-    done
+	done
 done
