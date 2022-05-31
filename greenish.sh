1d3c70851d7fafa066cb65c926a32c449059d7ec
Vivek-mishra-2002 <vivekmishra4002@gmail.com>
2022-06-01 01:00:00 +0530

    Never give an inch!

--- greenish.sh
+++ greenish.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #echo $var
 for ((DAYS = 1 ; DAYS <= 30 ; DAYS++)); do
-    sudo date -s "2022-05-$DAYS"
+    sudo date -s "2022-06-$DAYS"
     var=$((1 + $RANDOM % 22))
     for ((HOURS = 1 ; HOURS < $var ; HOURS++)); do
         echo HOURS $HOURS
-        sudo date -s "2022-05-$DAYS $HOURS:00:00"
+        sudo date -s "2022-06-$DAYS $HOURS:00:00"
         echo `/usr/games/fortune -sn 32` > vivek.txt
         git add .
         git commit -m "`/usr/games/fortune -sn 32`"
